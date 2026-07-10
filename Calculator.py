1de08c4bae2c20c4f3f27fa0210b362daec359ee
Apex-Shift <k-kriim@protonmail.com>
2026-07-10 12:04:14 +0200

    Implement basic calculator operations

---
+++ Calculator.py
@@ -0,0 +1,10 @@
+num_1 = int(input("Number 1:"))
+num_2 = int(input("Number 2:"))
+operation = str(input("Operation:"))
+
+if operation == "add" :
+    print(f"{num_1} + {num_2} = {num_1 + num_2}")
+if operation == "multiply" :
+    print(f"{num_1} * {num_2} = {num_1 * num_2}")
+if operation == "subtract" :
+    print(f"{num_1} - {num_2} = {num_1 - num_2}")
