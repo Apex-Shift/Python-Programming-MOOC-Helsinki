d3842a73ba2952f95c84e369c35f234267619a7b
Apex-Shift <k-kriim@protonmail.com>
2026-07-10 17:51:35 +0200

    Add age check functionality

---
+++ Age check.py
@@ -0,0 +1,9 @@
+# Write your solution here
+age = int(input("What is your age?"))
+
+if age >= 5 :
+    print("Ok, you're",age, "years old")
+if age < 5 and age >= 0:
+    print("I suspect you can't write quite yet...")
+if age < 0:
+    print("That must be a mistake")
