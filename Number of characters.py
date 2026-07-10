06a769c610eab9367bbf66c7b00b28d128f8d66f
Apex-Shift <k-kriim@protonmail.com>
2026-07-10 15:49:14 +0200

    Add script to count letters in a word

---
+++ Number of characters.py
@@ -0,0 +1,7 @@
+word = input("Please type in a word:")
+length = len(word)
+if length > 1:
+    print("There are",length, "letters in the word", word )
+    print("Thank you!")
+else:
+    print("Thank you!")
