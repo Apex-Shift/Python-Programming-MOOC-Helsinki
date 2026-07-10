26bdf1117e2488b2809dfd0b7af69b1946a13ec7
Apex-Shift <k-kriim@protonmail.com>
2026-07-10 11:24:11 +0200

    Add absolute value calculation in Absolute value.py

---
+++ Absolute value.py
@@ -0,0 +1,5 @@
+int_num = int(input("Please type in a number:"))
+if int_num < 0 :
+    print("The absolute value of this number is", int_num * -1)
+else: 
+    print("The absolute value of this number is", int_num)
