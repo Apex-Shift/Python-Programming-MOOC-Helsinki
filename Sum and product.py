91334655b7958b75eec94fc63ca57910c325c438
Apex-Shift <k-kriim@protonmail.com>
2026-07-10 10:24:06 +0200

    Add sum and product calculation in Sum and product.py

---
+++ Sum and product.py
@@ -0,0 +1,6 @@
+first_num = int(input("Number 1 :"))
+second_num = int(input("Number2 :"))
+product = first_num * second_num
+the_sum = first_num + second_num
+print("The sum of the numbers:", the_sum)
+print("The product of the numbers:", product)
