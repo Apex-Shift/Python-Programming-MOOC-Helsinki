da73194a10f99b67a8d969390fa36d53615429aa
Apex-Shift <k-kriim@protonmail.com>
2026-07-10 11:34:27 +0200

    Add soup ordering script

---
+++ Soup or no soup.py
@@ -0,0 +1,8 @@
+name = str(input("Please tell me your name:"))
+
+if name != "Jerry" :
+    portion = int(input("How many portions of soup?"))
+    print("The total cost is", portion * 5.90)
+    print("Next please!")
+else: 
+    print("Next please!")
