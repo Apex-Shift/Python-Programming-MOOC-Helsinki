ca45b9f831a28a2c49abd3e77c2911d764feeb5f
Apex-Shift <k-kriim@protonmail.com>
2026-07-10 10:24:43 +0200

    Add sum and mean calculation for four numbers

---
+++ Sum and mean.py
@@ -0,0 +1,7 @@
+num_1 = int(input("Number 1:"))
+num_2 = int(input("Number 2:"))
+num_3 = int(input("Number 3:"))
+num_4 = int(input("Number 4:"))
+the_sum = num_1 + num_2 + num_3 + num_4
+mean = the_sum / 4
+print("The sum of the numbers is", the_sum, "and the mean is", str(mean))
