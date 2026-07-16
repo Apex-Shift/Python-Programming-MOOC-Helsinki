8f81a508656375fa76405e71e0d86cd87cf7ca5b
Apex-Shift <k-kriim@protonmail.com>
2026-07-16 21:25:23 +0200

    Implement grading system based on points input

---
+++ Grades and points.py
@@ -0,0 +1,22 @@
+points = int(input("How many points [0-100]:"))
+if points < 0:
+    print("Grade: impossible!")
+
+if points >= 0 and points <= 49:
+    print("Grade: fail")
+
+if points >= 50 and points <= 59:
+    print("Grade: 1")
+
+if points >= 60 and points <= 69:
+    print("Grade: 2")
+if points >= 70 and points <= 79:
+    print("Grade: 3")
+if points >= 80 and points <= 89:
+    print("Grade: 4")
+if points >= 90 and points <= 100:
+    print("Grade: 5")
+if points > 100:
+    print("Grade : impossible!")
+
+
