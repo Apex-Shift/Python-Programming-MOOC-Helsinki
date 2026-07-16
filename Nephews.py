26e83bdd93769088c8941bea61c275327c5f4bd4
Apex-Shift <k-kriim@protonmail.com>
2026-07-16 19:55:12 +0200

    Add nephew identification based on user input

---
+++ Nephews.py
@@ -0,0 +1,8 @@
+name = str(input("Please type in your name: "))
+
+if name in ['Morty', 'Ferdie']:
+    print("I think you might be one of Mickey Mouse's nephews.")
+elif name in ['Huey', 'Dewey', 'Louie']:
+    print("I think you might be one of Donald Duck's nephews.")
+else:
+    print("You're not a nephew of any character I know of.")
