09dc37a56174c88022a712cd63ad7748285f7b9e
Apex-Shift <k-kriim@protonmail.com>
2026-07-17 13:12:07 +0200

    Create Story2.py

---
+++ Story2.py
@@ -0,0 +1,11 @@
+story = ""
+previous_word = ""
+
+while True:
+    word = input("Please type in a word: ")
+    if word == "end" or word == previous_word:
+        break
+    story += word + " "
+    previous_word = word
+
+print(story.strip())
