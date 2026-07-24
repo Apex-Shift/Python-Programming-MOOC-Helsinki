c98c9cf3ec86e7fd85cafd91befaf44483f61387
Apex-Shift <k-kriim@protonmail.com>
2026-07-24 14:44:25 +0200

    Add script to compare second and second to last characters

---
+++ Second and second to last characters.py
@@ -0,0 +1,6 @@
+mot = input("Please type in a string: ")
+
+if mot[1] == mot[-2]:
+    print("The second and the second to last characters are " + mot[1])
+else:
+    print("The second and the second to last characters are different")
