a44d401e89b861391350458d282142ddeb1aa56e
Apex-Shift <k-kriim@protonmail.com>
2026-08-01 17:14:04 +0200

    Create Seven Brothers2.py

---
+++ Seven Brothers2.py
@@ -0,0 +1,9 @@
+def seven_brothers():
+    # Print the names of the seven brothers in alphabetical order
+    print("Aapo")
+    print("Eero")
+    print("Juhani")
+    print("Lauri")
+    print("Simeoni")
+    print("Timo")
+    print("Tuomas")
