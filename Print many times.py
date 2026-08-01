4593f2014af0164a4f463902a80095d1a374ee45
Apex-Shift <k-kriim@protonmail.com>
2026-08-01 17:23:02 +0200

    Create Print many times.py

---
+++ Print many times.py
@@ -0,0 +1,8 @@
+def print_many_times(text, times):
+    # Une boucle pour imprimer le texte le nombre de fois demandé
+    for i in range(times):
+        print(text)
+
+# You can test your function by calling it within the following block
+if __name__ == "__main__":
+    print_many_times("hi", 5)
