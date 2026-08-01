995d4434d32fa20805e21398b68f4c6994545653
Apex-Shift <k-kriim@protonmail.com>
2026-08-01 17:21:16 +0200

    Add mean function to calculate arithmetic mean

---
+++ Mean.py
@@ -0,0 +1,8 @@
+def mean(num1, num2, num3):
+    # Calculate the arithmetic mean and print it
+    result = (num1 + num2 + num3) / 3
+    print(result)
+
+# Testing the function
+if __name__ == "__main__":
+    mean(1, 2, 3)
